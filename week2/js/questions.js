b893063bf73846880d1cdaf83889c0f9826ecfdc
Kushagra-Sikka <kushagrasikka05@gmail.com>
2022-03-10 15:39:17 +0530

    Week 2 task

---
+++ week2/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What characters are allowed in a C function name identifier?",
+    answer: "Alphabets, Numbers, Underscore ( _ )",
+    options: [
+      "Alphabets, Numbers, %, $, _",
+      "Alphabets, Numbers, %",
+      "Alphabets, Numbers, Underscore ( _ )",
+      "Alphabets, Numbers, dollar $"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Recursion is similar to which of the following?",
+    answer: "Loop",
+    options: [
+      "Switch Case",
+      "Loop",
+      "if-else",
+      "None"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is a Structure in C?",
+    answer: "All",
+    options: [
+      "A structure is a collection of elements that can be of same data type",
+      " A structure is a collection of elements that can be of different datatype",
+      "It is a user defined data type",
+      "All"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following is false?",
+    answer: "All recursive functions must have a return value",
+    options: [
+      "In C any function can make a recursive call",
+      "All recursive functions must have a return value",
+      "A recursive function can call itself infinitely",
+      "There is no special indication for recursive function other than calling itself in the function body"
+    ]
+  },
+  {
+    numb: 7,
+    question: "How many sizes of headers are available in HTML by default?",
+    answer: "6",
+    options: [
+      "5",
+      "3",
+      "1",
+      "6"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the smallest header in HTML by default?",
+    answer: "h6",
+    options: [
+      "h1",
+      "h2",
+      "h6",
+      "h3"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The property in CSS used to change the background color of an element is-",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The CSS property used to control the element's font-size is -",
+    answer: "font-size",
+    options: [
+      "text-style",
+      "font-size",
+      "text-size",
+      "None of the above"
+    ]
+  },
+];
